139b5799f626eb4c7cb9382e56ee49c0bad6aa65
huangzhaonan <huangzhaonan@wanda.cn>
2017-11-07 23:00:49 +0800

    m

--- jsdev/console.js
+++ jsdev/console.js
@@ -2,7 +2,7 @@ import {ajax} from "./variable.js"
 ajax({
     path: 'http://api.ffan.com/wechatxmt/v1/plaza/floors',
     param: {
-        plazaId:1000274,
+        plazaId:1,
         border:[1,2,3]
     },
     cache:true,
@@ -15,11 +15,56 @@ ajax({
     }
 });
 
+ajax({
+    path: 'http://api.ffan.com/wechatxmt/v1/plaza/floors',
+    param: {
+        plazaId:2,
+        border:[1,2,3]
+    },
+    cache:true,
+    timeout:1000,
+    before: () => {
+
+    },
+    success: (res) => {
+
+    }
+});
+ajax({
+    path: 'http://api.ffan.com/wechatxmt/v1/plaza/floors',
+    param: {
+        plazaId:1,
+        border:[1,2,3]
+    },
+    cache:true,
+    timeout:1000,
+    before: () => {
+
+    },
+    success: (res) => {
+
+    }
+});
+ajax({
+    path: 'http://api.ffan.com/wechatxmt/v1/plaza/floors',
+    param: {
+        plazaId:3,
+        border:[1,2,3]
+    },
+    cache:true,
+    timeout:1000,
+    before: () => {
+
+    },
+    success: (res) => {
+
+    }
+});
 setTimeout(function () {
     ajax({
         path: 'http://api.ffan.com/wechatxmt/v1/plaza/floors',
         param: {
-            plazaId:1000224,
+            plazaId:1,
             border:[1,2,3]
         },
         cache:true,
