f79598c868c20264ae8a65d1a3b705785bf3e07d
huangzhaonan <huangzhaonan@wanda.cn>
2017-11-07 17:00:47 +0800

    add tree

--- jsdev/console.js
+++ jsdev/console.js
@@ -1,19 +1,38 @@
 import {ajax} from "./variable.js"
 ajax({
-    path: 'wechatxmt/v1/userCard/memberInfo',
+    path: 'http://api.ffan.com/wechatxmt/v1/plaza/floors',
     param: {
-        relationId:11
+        plazaId:1000274,
+        border:[1,2,3]
     },
+    cache:true,
+    timeout:1000,
     before: () => {
 
     },
-    after: () => {
-
-    },
     success: (res) => {
-        if(res.status == 200){
+        console.log(res)
 
-        }
     }
-})
+});
 
+setTimeout(function () {
+    ajax({
+        path: 'http://api.ffan.com/wechatxmt/v1/plaza/floors',
+        param: {
+            plazaId:1000224,
+            border:[1,2,3]
+        },
+        cache:true,
+        before: () => {
+
+        },
+        success: (res) => {
+            console.log(res)
+
+        },
+        fail: (error) => {
+            console.log(error)
+        }
+    });
+},3000);
--- jsdev/variable.js
+++ jsdev/variable.js
@@ -21,15 +21,19 @@ const paramType = (data,type) => {
 
 
 const ajax = (json) => {
+
     let params = {
-        path : json.path || "",
-        param : json.param || {},
-        success : json.success || function () {},
-        type : json.type || "GET",
-        outLand : json.outLand || false,
-        before : json.before || function () {},
-        after : json.after || function () {}
+        path : json.path || "",//ajax请求url
+        param : json.param || {},//请求参数
+        success : json.success || function () {},//调用成功回调函数
+        saveData : json.saveData || function () {},//需要储存的数据
+        type : json.type || "GET",//请求类型
+        cache : json.cache || false,//是否读缓存
+        before : json.before || function () {},//请求前回调函数
+        fail : json.fail || function () {},//请求失败回调函数
+        timeout : json.timeout || 0,//缓存过期时间
     };
+    //执行ajax前的before
     params.before();
     let url = "";
     //数据结构
@@ -43,7 +47,7 @@ const ajax = (json) => {
         },
         mode: 'cors'
     };
-    //交互方式以及传参方式
+    //GET参数处理
     if(params.type =="GET"){
         for (var i in params.param) {
             //如果是json,转成字符串
@@ -56,20 +60,68 @@ const ajax = (json) => {
                 params.param[i] = JSON.stringify(params.param[i]);
             }
         }
-        url = (params.outLand ? "" : "http://api.ffan.com/") + params.path + paramType(params.param,"GET");
+        url =  params.path + paramType(params.param,"GET");
     }
+    //POST参数处理
     if(params.type =="POST"){
-        url = (params.outLand ? "" : "http://api.ffan.com/") + params.path;
+        url =  params.path;
         let paramsString = paramType(params.param,"POST");
         fetchBody.body = paramsString;
     }
-    return fetch(url,fetchBody)
-        .then(response => response.json())
-        .then(params.success)
-        .then(params.after)
-        .catch(error => console.log(error))
 
+
+    //检验urlList里有没有这条url
+    let saveUrl = false;
+    let offset = null;
+    crayon.urlList.forEach((item,i)=>{
+        if(params.path + paramType(params.param,"GET") == item.url){
+            saveUrl = true;
+            offset = i;
+        }
+    });
+    //如果有缓存读缓存,没有读接口
+    if(params.cache && saveUrl){
+        return  params.success(crayon.data[offset]);//回调success
+    }else{
+        return fetch(url,fetchBody)
+            .then(response => response.json())
+            .then((res) =>{
+                const data = res;//params.saveData(res);//保存数据
+                //如果urlList里没有这个url,那就把这个url塞进urllist,并把数据存进crayon的data
+                if(!saveUrl){
+                    crayon.urlList.push({url:params.path + paramType(params.param,"GET"),time:Date.parse(new Date())});
+                    crayon.save(data,crayon.data.length);
+                }
+                //过期清理缓存
+                if(params.timeout != 0){
+                    setTimeout(()=>{
+                        delete crayon.urlList[crayon.urlList.length-1];
+                        delete crayon.data[crayon.data.length-1];
+                    },params.timeout)
+                }
+                params.success(res);//回调success
+            })
+            .catch(error => params.fail(error))
+    }
+};
+export const crayon = {};
+
+crayon.data = [];
+crayon.urlList = [];
+
+crayon.save = (res,offset) =>{
+    crayon.data[offset] = res;
+    // console.log(crayon.data)
 };
 
 
-export {ajax}
+
+
+
+
+
+
+
+
+
+export {ajax,crayon}
